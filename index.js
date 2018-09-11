391a63624dc4ba25a9d1d5b3135a2c2faeb1610f
Loryn Mason <masonloryn@gmail.com>
2018-09-11 14:57:46 -0600

    Fix delete button for real this time

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ function checkInputs() {
 }
 
 function upDownVoting() {
+    if ($(event.target).hasClass('upvote') || $(event.target).hasClass('downvote') ) {
     var cardObject = JSON.parse(localStorage.getItem($(event.target).parent().prop('id')));
     if ($(event.target).hasClass('upvote') && cardObject.quality < 2) {
        cardObject.quality++; 
@@ -79,26 +80,15 @@ function upDownVoting() {
     localStoreCard(cardObject.id, cardObject); 
     $(event.target).parent().remove();  
 }
-
-function deleteCard() {
-    if ($(event.target).hasClass('delete-button')) {
-    var id = $(event.target).parent().prop('id');
-        localStorage.removeItem(id);
-    var cardList = $(event.target).closest('.card-container').remove();
-        // $(event.target).parent().remove();
-    }
 }
 
-// function deleteCard() {
-//     var id = $(event.target).parent().prop('id');
-//     var cardList = $('.card-container')
-//     if ($(event.target).hasClass('delete-button')) {
-//         $(event.target).parent().remove();
-//         // cardList.addClass('hidden');
-//         localStorage.removeItem(id);
-//     }
-// }
-
+function deleteCard(event) {
+    if ($(event.target).hasClass('delete-button')) {
+    var id = $(event.target).parent().attr('id');
+    $(event.target).parents('.card-container').remove()
+    localStorage.removeItem(id);
+    }
+}
 
 function clearInputs() {
     $('#title-input').val('');
@@ -106,70 +96,16 @@ function clearInputs() {
     $('.save-btn').attr('disabled', true);
 }
 
-function searchBar() {
-    var cardList = $('.card-container')
-    var searchFilter = $('#search-input').val().toLowerCase();
-    for(var i = 0; i < cardList.length; i++) {
-        var title = $(cardList[i]).children('.title-of-card')[0].text();
-        var body = $(cardList[i]).children('.title-of-card')[0].text();;
-    if(title.toLowerCase().includes(searchFilter) || body.toLowerCase().includes(searchFilter)) {
-        cardList.removeClass('hide-card');
-    } else {
-        cardList.addClass('hide-card');
-    }
-    }
-}
-
-        // if (event.target.className === "upvote" && currentQuality === "plausible"){
-        //     qualityVariable = "genius";
-        //     $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-               
-        // } else if (event.target.className === "upvote" && currentQuality === "swill") {
-        //     qualityVariable = "plausible";
-        //     $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-               
-        // } else if (event.target.className === "downvote" && currentQuality === "plausible") {
-        //     qualityVariable = "swill"
-        //     $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-        // } else if (event.target.className === "downvote" && currentQuality === "genius") {
-        //     qualityVariable = "plausible"
-        //     $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-        // } else if (event.target.className === "downvote" && currentQuality === "swill") {
-        //     qualityVariable = "swill";
-        
-        // } else if (event.target.className === "upvote" && currentQuality === "genius") {
-        //     qualityVariable = "genius";
-        // }
-
-    // var cardHTML = $(event.target).closest('.card-container');
-    // var cardHTMLId = cardHTML[0].id;
-    // var cardObjectInJSON = localStorage.getItem(cardHTMLId);
-    // var cardObjectInJS = JSON.parse(cardObjectInJSON);
-
-    // cardObjectInJS.quality = qualityVariable;
-
-    // var newCardJSON = JSON.stringify(cardObjectInJS);
-    // localStorage.setItem(cardHTMLId, newCardJSON);
-    // }
-   
-    // else if (event.target.className === "delete-button") {
-    //     var cardHTML = $(event.target).closest('.card-container').remove();
-    //     var cardHTMLId = cardHTML[0].id;
-    //     localStorage.removeItem(cardHTMLId);
-    // }
-// });
-
-
-      
-
-
-
-
-
-
-
-
-
-
+// function searchBar() {
+//     var cardList = $('.card-container')
+//     var searchFilter = $('#search-input').val().toLowerCase();
+//     for(var i = 0; i < cardList.length; i++) {
+//         var title = $(cardList[i]).children('.title-of-card')[0].text();
+//         var body = $(cardList[i]).children('.body-of-card')[0].text();;
+//     if(title.toLowerCase().includes(searchFilter) || body.toLowerCase().includes(searchFilter)) {
+//         cardList.removeClass('hide-card');
+//     } else {
+//         cardList.addClass('hide-card');
+//     }
+//     }
+// }
